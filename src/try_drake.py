716fa26f13e6a14183044cd9d7331fa11efcca18
ZhenyuWu323 <wzy2575765661@gmail.com>
2025-03-26 13:33:56 -0700

    finally drake

--- src/try_drake.py
+++ src/try_drake.py
@@ -41,25 +41,31 @@ def convert_all_stl_to_obj(mesh_dir):
     print(f"Exporting Completer: Success {success_count}/{len(stl_files)}")
 
 
-def build(urdf_path):
-    urdf_path = Path(urdf_path)
-    
-    # 读取原始 URDF 内容
-    with open(urdf_path, "r") as f:
-        content = f.read()
+def map_dof_idx_to_drake_q(dof_idx, joint_names):
+    name_to_drake_q_index = {
+        'r_wheel_joint': 7,
+        'l_wheel_joint': 8,
+        'torso_lift_joint': 9,
+        'head_pan_joint': 10,
+        'head_tilt_joint': 11,
+        'shoulder_pan_joint': 12,
+        'shoulder_lift_joint': 13,
+        'upperarm_roll_joint': 14,
+        'elbow_flex_joint': 15,
+        'forearm_roll_joint': 16,
+        'wrist_flex_joint': 17,
+        'wrist_roll_joint': 18,
+        'r_gripper_finger_joint': 19,
+        'l_gripper_finger_joint': 20,
+    }
+    return [name_to_drake_q_index[joint_names[i]] for i in dof_idx]
 
-    # 替换 .STL 为 .obj
-    content_new = content.replace(".STL", ".obj")
-
-    # 生成新的 URDF 文件路径
-    new_urdf_path = urdf_path.with_name(urdf_path.stem + "_drake.urdf")
-
-    # 写入新的 Drake 兼容 URDF 文件
-    with open(new_urdf_path, "w") as f:
-        f.write(content_new)
 
 
-def compute_fk(urdf_path, joint_positions, T_world_base_4x4, eef_name, dof_idx):
+def compute_fk(urdf_path, joint_positions, T_world_base_4x4, eef_name, dof_idx, og_joint_name):
+
+
+
     # 读取 URDF（支持 STL 替换为 OBJ）
     urdf_path = Path(urdf_path)
     with open(urdf_path, "r") as f:
@@ -77,7 +83,8 @@ def compute_fk(urdf_path, joint_positions, T_world_base_4x4, eef_name, dof_idx):
     context = plant.CreateDefaultContext()
     nq = plant.num_positions(model_index)
     q_all = np.zeros(nq)
-    q_all[dof_idx] = joint_positions
+    idx = map_dof_idx_to_drake_q(dof_idx, og_joint_name)
+    q_all[idx] =joint_positions
 
     plant.SetPositions(context, model_index, q_all)
 
@@ -92,5 +99,48 @@ def compute_fk(urdf_path, joint_positions, T_world_base_4x4, eef_name, dof_idx):
     eef_body = plant.GetBodyByName(eef_name, model_index)
     T_world_eef = plant.EvalBodyPoseInWorld(context, eef_body)
     pos,ori = T.mat2pose(T_world_eef.GetAsMatrix4())
+    
+    #name = plant.GetPositionNames(model_index)
+    #print(f"Position #{model_index} -> {name}")
+
     print(pos)
     return T_world_eef
+
+'''dict_keys(['l_wheel_joint', 8
+           'r_wheel_joint', 7
+           'torso_lift_joint', 9
+           'head_pan_joint', 10
+           'shoulder_pan_joint', 12
+           'head_tilt_joint', 11
+           'shoulder_lift_joint', 13
+           'upperarm_roll_joint', 14
+           'elbow_flex_joint', 15
+           'forearm_roll_joint', 16
+           'wrist_flex_joint', 17
+           'wrist_roll_joint', 18
+           'l_gripper_finger_joint', 19
+           'r_gripper_finger_joint'])20
+'''
+
+
+'''['base_link_qw', 
+ 'base_link_qx', 
+ 'base_link_qy', 
+ 'base_link_qz', 
+ 'base_link_x', 
+ 'base_link_y', 
+ 'base_link_z', 
+ 'r_wheel_joint_q', 
+ 'l_wheel_joint_q', 
+ 'torso_lift_joint_x', 
+ 'head_pan_joint_q', 
+ 'head_tilt_joint_q', 
+ 'shoulder_pan_joint_q', 
+ 'shoulder_lift_joint_q', 
+ 'upperarm_roll_joint_q', 
+ 'elbow_flex_joint_q', 
+ 'forearm_roll_joint_q', 
+ 'wrist_flex_joint_q', 
+ 'wrist_roll_joint_q', 
+ 'r_gripper_finger_joint_x', 
+ 'l_gripper_finger_joint_x']'''
